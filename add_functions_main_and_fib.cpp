f25611d9db5eac78ac9e0fa14cb0ea56500e1c6f
unknown <smamedova567@gmail.com>
2024-05-27 20:49:44 +0400

    add_functions_main_and_fib.cpp

---
+++ add_functions_main_and_fib.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int fib(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    int main()
+    {
+        int n = 10;
+        for (int i = 1; i <= n; i++) {
+            cout << fib(i) << endl;
+        }
+    }
